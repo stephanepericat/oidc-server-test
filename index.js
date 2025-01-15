635c20d45b1f48e47d485181f352e6e7d95df60d
Stephane Pericat <shortwavlabs@Mac.attlocal.net>
2025-01-14 18:00:10 -0600

    prettier

--- index.js
+++ index.js
@@ -2,26 +2,28 @@ import Provider from 'oidc-provider'
 import express from 'express'
 
 const configuration = {
-  clients: [{
-     client_id: "oidcCLIENT",      
-     client_secret: "Some_super_secret",      
-     grant_types: ["authorization_code"],      
-     redirect_uris: [ "https://oidcdebugger.com/debug"], 
-     response_types: ["code"],  
-       
-   //other configurations if needed
-  }],
+  clients: [
+    {
+      client_id: 'oidcCLIENT',
+      client_secret: 'Some_super_secret',
+      grant_types: ['authorization_code'],
+      redirect_uris: ['https://oidcdebugger.com/debug'],
+      response_types: ['code'],
+
+      //other configurations if needed
+    },
+  ],
   pkce: {
     required: () => false,
   },
-};
+}
 
 const oidc = new Provider('http://localhost:8080', configuration)
 
-const app = express();
+const app = express()
 
-app.use("/oidc",oidc.callback());
+app.use('/oidc', oidc.callback())
 
 app.listen(8080, function () {
-  console.log('OIDC is listening on port 8080!');
-});
+  console.log('OIDC is listening on port 8080!')
+})
